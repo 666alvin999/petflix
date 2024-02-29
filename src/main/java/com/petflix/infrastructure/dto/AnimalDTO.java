2bb4a9d9cc4c6de000741e6a9a54a1f212454a7b
Alvin Hamaïde <alvin98mc@gmail.com>
2024-02-29 08:11:42 +0100

    Updated sql files for tests

---
+++ src/main/java/com/petflix/infrastructure/dto/AnimalDTO.java
@@ -0,0 +1,19 @@
+package com.petflix.infrastructure.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class AnimalDTO {
+
+	private int id;
+	private String name;
+	private String type;
+	private int age;
+	private String url;
+
+}
--- src/test/resources/animal_init.sql
+++ src/test/resources/animal_init.sql
@@ -1,8 +1,16 @@
-CREATE TABLE ANIMAL (
-    ID                 INT AUTO_INCREMENT PRIMARY KEY,
-    NAME               VARCHAR(30) NOT NULL,
-    TYPE               VARCHAR(20) NOT NULL,
-    AGE                INT         NOT NULL,
-    PRESENTATION_VIDEO INT         NOT NULL,
-    MANAGING_MEMBER    INT         NOT NULL
+DROP TABLE IF EXISTS ANIMAL;
+
+CREATE TABLE ANIMAL
+(
+    ID               INT PRIMARY KEY,
+    NAME             VARCHAR(30)  NOT NULL,
+    TYPE             VARCHAR(20)  NOT NULL,
+    AGE              INT          NOT NULL,
+    PRESENTATION_URL VARCHAR(512) NOT NULL,
+    MANAGING_MEMBER  INT          NOT NULL
 );
+
+INSERT INTO ANIMAL
+VALUES (0, 'Oslo', 'chat', 3, 'https://www.url1.com', 0),
+       (1, 'Uta', 'chat', 1, 'https://www.url1.com', 0),
+       (2, 'Maul', 'chien', 4, 'https://www.url2.com', 0);
